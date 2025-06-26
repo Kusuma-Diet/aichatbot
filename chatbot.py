6e4b02fdbf1dc0d9d53fcd9606415fdb3e0cd8f0
sreelekha009 <sreelekhaediga09@gmail.com>
2025-06-27 00:53:46 +0530

    Fix: bind Flask to 0.0.0.0 and use dynamic PORT for Render

--- chatbot.py
+++ chatbot.py
@@ -10,6 +10,12 @@ from werkzeug.utils import secure_filename
 import PyPDF2
 
 app = Flask(__name__)
+app.run(debug=True)
+
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
 
 # Configure upload folder
 UPLOAD_FOLDER = 'uploads'
